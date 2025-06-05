326a1fe49894706467c91c1326601efbb10c914e
davidelong0 <davidelong0@hotmail.com>
2025-06-05 15:06:56 +0200

    Initial commit

---
+++ src/main/java/it/epicode/u2s5l4/U2s5l4Application.java
@@ -0,0 +1,13 @@
+package it.epicode.u2s5l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2s5l4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2s5l4Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/u2s5l4/U2s5l4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.u2s5l4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2s5l4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
